a1418564740eb43d750c3eef24650357960e84fd
Tony Summerville <tsummerville@rarestep.com>
2013-09-08 23:02:13 -0500

    Exported site

--- exported/js/application.js
+++ exported/js/application.js
@@ -1,13 +1,5 @@
 (function() {
 
-  $(function() {
-    return $("#twitter").tweet({
-      join_text: null,
-      username: "yesjptsc",
-      count: 3,
-      template: "{avatar}{text}<br/>{time}",
-      loading_text: "Loading tweets..."
-    });
-  });
+
 
 }).call(this);
